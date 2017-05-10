632cfb701858117e89482075b33b37961c3abc0f
yufeng <e0003228@u.nus.edu>
2017-05-10 22:45:07 +0800

    add enemySpawnerManager script

---
+++ Assets/Scripts/EnemySpawnerManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerManager : MonoBehaviour {
+
+	[System.Serializable]
+	private class MonsterGroup {
+		[SerializeField]public GameObject monsterType;
+		[SerializeField]public int monsterAmount;
+		[SerializeField]public float spawnTimer;
+		[SerializeField]public bool isRandom;
+		[SerializeField]public GameObject[] spawnpoints;
+	}
+
+	[System.Serializable]
+	private class MonsterWave {
+		[SerializeField]public MonsterGroup[] monsterGroupList;
+	}
+
+
+//================================================== Main class information =========================================================//
+
+
+	[SerializeField]
+	private MonsterWave[] monsterWaveList;
+	private GameObject[] spawnpointList;
+	private int nextWaveIndex = 0;
+
+
+	public void spawnNextWave() {
+		MonsterWave targetWave = monsterWaveList [nextWaveIndex];
+		foreach(MonsterGroup grp in targetWave.monsterGroupList){
+			StartCoroutine (spawnGroup (grp));
+		}
+		nextWaveIndex = (nextWaveIndex + 1) % monsterWaveList.Length;
+	}
+
+	IEnumerator spawnGroup(MonsterGroup grp) {
+		yield return new WaitForSeconds (grp.spawnTimer);
+		if (!grp.isRandom && grp.spawnpoints.Length == 0) {
+			Instantiate (grp.monsterType, spawnpointList[0].transform.position, Quaternion.identity);
+		}
+		for (int n = 0; n < grp.monsterAmount; n++) {
+			Vector2 spawnLocation = grp.isRandom? 
+				spawnpointList[Random.Range(0, spawnpointList.Length)].transform.position: 
+				grp.spawnpoints[Random.Range(0, grp.spawnpoints.Length)].transform.position;
+			Instantiate (grp.monsterType, spawnLocation, Quaternion.identity);
+		}
+	}
+}
