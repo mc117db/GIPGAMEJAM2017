d0a57814652cd8c1ffe1981c915f8712b8e2f704
Jon Jubelag <mc117db@gmail.com>
2017-05-10 15:36:04 +0800

    Added BulletBehaviour + Pooling

---
+++ Assets/Scripts/Behaviour/BulletBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class BulletBehaviour : MonoBehaviour, IRecycle {
+
+    Vector2 bulletVelocity;
+    float speed;
+    Rigidbody2D rigidbodyComponent;
+	// Update is called once per frame
+    public void Initialize(Vector2 vel,float spd)
+    {
+        rigidbodyComponent = gameObject.GetComponent<Rigidbody2D>();
+        bulletVelocity = vel.normalized;
+        speed = spd;
+        rigidbodyComponent.velocity = bulletVelocity * speed;
+    }
+	void Update () {
+        if (!CheckIfOffScreen())
+        {
+            GameObjectUtil.Destroy(gameObject);
+        }
+	}
+    bool CheckIfOffScreen()
+    {
+        Vector3 bulletScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        if (bulletScreenPoint.x < 0 || bulletScreenPoint.x > Screen.width || bulletScreenPoint.y < 0 || bulletScreenPoint.y > Screen.height)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    // Object pooler interface implementations
+    public void Restart()
+    {
+        bulletVelocity = Vector2.zero;
+        speed = 0;
+    }
+
+    public void Shutdown()
+    {
+        bulletVelocity = Vector2.zero;
+        speed = 0;
+    }
+}
---
+++ Assets/Scripts/Behaviour/FireBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBehaviour {
+    public GameObject bulletPrefab;
+    public void FireBasedOnAffinity(int affinityPower)
+    {
+        switch (affinityPower)
+        {
+            case 1:
+                FireLevelOne();
+                break;
+            case 2:
+                FireLevelTwo();
+                break;
+            case 3:
+                FireLevelThree();
+                break;
+            case 4:
+                FireLevelFour();
+                break;
+            case 5:
+                FireLevelFive();
+                break;
+            default:
+                break;
+        }
+    }
+    #region Firing Implementations
+    protected virtual void FireLevelOne()
+    {
+
+    }
+    protected virtual void FireLevelTwo()
+    {
+
+    }
+    protected virtual void FireLevelThree()
+    {
+
+    }
+    protected virtual void FireLevelFour()
+    {
+
+    }
+    protected virtual void FireLevelFive()
+    {
+
+    }
+    #endregion
+}
