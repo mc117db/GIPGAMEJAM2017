942e0a6fb3bf7aea83a573929e30184051af9fad
yufeng <e0003228@u.nus.edu>
2017-05-11 01:01:55 +0800

    add resuable audio & blink effects

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour {
+
+	public AudioClip[] audioClip;
+	AudioSource audioSource;
+
+	void Start() {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void playOnce(int index) {
+		this.audioSource.PlayOneShot (audioClip [index]);
+	}
+
+	public void playLoop(int index) {
+		this.audioSource.Stop ();
+		this.audioSource.loop = true;
+		this.audioSource.clip = audioClip [index];
+		this.audioSource.Play ();
+	}
+
+	public void stopAudio() {
+		this.audioSource.Stop ();
+	}
+}
---
+++ Assets/Scripts/BlinkingEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BlinkingEffect : MonoBehaviour {
+
+	private Color mainColor;
+	private float recoverRate = 10f;
+
+	void Start() {
+		mainColor = GetComponent<SpriteRenderer> ().color;
+	}
+
+	void Update() {
+		mainColor.a = Mathf.Lerp (mainColor.a, 1f, recoverRate * Time.deltaTime);
+	}
+
+	void blinkOnce() {
+		mainColor.a = 0.1f;
+	}
+
+	void blinkMultiple() {
+		StartCoroutine (blinkMultipleEffects ());
+	}
+
+	IEnumerator blinkMultipleEffects() {
+		for (int n = 0; n < 5; n++) {
+			mainColor.a = 0.1f;
+			yield return new WaitForSeconds (0.1f);
+		}
+		yield return null;
+	}
+}
