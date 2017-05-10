92170bd99b4bccd6867ad005bdb5d28f8f5de804
yufeng <e0003228@u.nus.edu>
2017-05-10 18:08:34 +0800

    add playerInput and playermovement script

---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour {
+
+	[HideInInspector]public bool canMove;
+
+	[HideInInspector]public static bool upButtonDown;
+	[HideInInspector]public static bool downButtonDown;
+	[HideInInspector]public static bool leftButtonDown;
+	[HideInInspector]public static bool rightButtonDown;
+	[HideInInspector]public static bool spaceButtonPressed;
+	[HideInInspector]public static bool leftMouseDown;
+	[HideInInspector]public static bool rightMouseDown;
+
+	private bool prevLeftDown = false;
+	private bool prevRightDown = false;
+
+	void Start() {
+		canMove = true;
+	}
+
+	void Update () {
+		if (canMove) {
+			upButtonDown = Input.GetKey (KeyCode.W);
+			downButtonDown = Input.GetKey (KeyCode.S);
+			leftButtonDown = Input.GetKey (KeyCode.A);
+			rightButtonDown = Input.GetKey (KeyCode.D);
+			spaceButtonPressed = Input.GetKeyDown (KeyCode.Space);
+			checkMouseButton ();
+		}
+	}
+
+	// deals with mouse button inputs.
+	// if both mouse buttons are held down, check which one is the last to held down.
+	private void checkMouseButton() {
+		if (Input.GetKey (KeyCode.Mouse0) && Input.GetKey (KeyCode.Mouse1)) {
+			if (prevLeftDown == false && prevRightDown == true) {
+				leftMouseDown = prevLeftDown = prevRightDown = true;
+				rightMouseDown = false;
+			} else if (prevLeftDown == true && prevRightDown == false) {
+				rightMouseDown = prevRightDown = prevLeftDown = true;
+				leftMouseDown = false;
+			} else if (prevLeftDown == false && prevRightDown == false) {
+				// if player happen to press both mouse button in same frame, we pick left button.
+				leftMouseDown = prevLeftDown = prevRightDown = true;
+				rightMouseDown = false;
+			} else {
+				return;
+			}
+		} else {
+			if (Input.GetKey (KeyCode.Mouse0) && !Input.GetKey (KeyCode.Mouse1)) {
+				leftMouseDown = prevLeftDown = true;
+				rightMouseDown = prevRightDown = false;
+			} else if (Input.GetKey (KeyCode.Mouse1) && !Input.GetKey (KeyCode.Mouse0)) {
+				leftMouseDown = prevLeftDown = false;
+				rightMouseDown = prevRightDown = true;
+			} else {
+				leftMouseDown = rightMouseDown = prevLeftDown = prevRightDown = false;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour {
+
+	[SerializeField][Range(0f, 200f)]private float movementSpeed = 50f;
+	[SerializeField][Range(0f, 10f)]private float maxSpeed = 2f;
+	[SerializeField]private float stoppingDamp = 5f;
+
+	private readonly float speedDamp = 0.5f;
+	private Rigidbody2D rbody;
+
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update() {
+		if (!PlayerInputs.leftButtonDown && !PlayerInputs.rightButtonDown &&
+			!PlayerInputs.upButtonDown && !PlayerInputs.downButtonDown) {
+			rbody.drag = stoppingDamp;
+		} else {
+			rbody.drag = 0;
+		}
+	}
+
+	void FixedUpdate () {
+		if (PlayerInputs.leftButtonDown) {
+			rbody.AddForce (new Vector2(-movementSpeed, 0));
+		}
+		if (PlayerInputs.rightButtonDown) {
+			rbody.AddForce (new Vector2(movementSpeed, 0));
+		}
+		if (PlayerInputs.upButtonDown) {
+			rbody.AddForce (new Vector2(0, movementSpeed));
+		}
+		if (PlayerInputs.downButtonDown) {
+			rbody.AddForce (new Vector2(0, -movementSpeed));
+		}
+
+		limitMovementSpeed ();
+	}
+
+	void limitMovementSpeed() {
+		if (rbody.velocity.magnitude > maxSpeed) {
+			Vector2 newSpeed = rbody.velocity.normalized * maxSpeed;
+			rbody.velocity = Vector2.Lerp (rbody.velocity, newSpeed, speedDamp);
+		} 
+	}
+}
